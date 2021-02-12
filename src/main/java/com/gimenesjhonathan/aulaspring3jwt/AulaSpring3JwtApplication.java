cf1006fa3c1704f18e42757ee24dc331e598f69b
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-12 20:05:52 -0300

    Primeiro commit

---
+++ src/main/java/com/gimenesjhonathan/aulaspring3jwt/AulaSpring3JwtApplication.java
@@ -0,0 +1,13 @@
+package com.gimenesjhonathan.aulaspring3jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpring3JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpring3JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimenesjhonathan/aulaspring3jwt/AulaSpring3JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimenesjhonathan.aulaspring3jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpring3JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
